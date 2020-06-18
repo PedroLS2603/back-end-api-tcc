0d30622b40ea93db0a08dcc54bddc72f670b60f2
Pedro Lima <pedro.l2002@hotmail.com>
2020-06-18 16:16:52 -0300

    Rotas ApartamentoTable criadas e funcionais

--- app/api/apartamento/apartamento_routes.py
+++ app/api/apartamento/apartamento_routes.py
@@ -1,8 +1,75 @@
 from flask import Blueprint, jsonify, request
 from __init__ import db
 from flask_sqlalchemy import SQLAlchemy
-from serializer import ApartamentoSchema
-from models import ApartamentoTable
+from serializer import ApartamentoSchema, PredioSchema
+from models import ApartamentoTable, PredioTable
 
 bp_apartamento = Blueprint('apartamento', __name__)
 
+@bp_apartamento.route('/apartamento/criar', methods=['POST'])
+def create():
+    
+    idpredio = request.json['predio']
+
+    predio = PredioTable.query.get(idpredio)
+
+
+    new_apt = ApartamentoTable(predio)
+
+    db.session.add(new_apt)
+    db.session.commit()
+
+    return jsonify('Tudo certo')
+
+@bp_apartamento.route('/apartamento/alterar/<id>', methods=['PUT'])
+def modify(id):
+
+    apt = ApartamentoTable.query.get(id)
+
+    idpredio = request.json['predio']
+
+    predio = PredioTable.query.get(idpredio)
+
+    apt.apartamento_idprd = predio.id
+
+    db.session.commit()
+
+    return jsonify('Tudo certo')
+
+@bp_apartamento.route('/apartamento/mostrar', methods=['GET'])
+def show_all():
+
+    apts = ApartamentoTable.query.all()
+    apartamentos = []
+
+    cont = 0 
+
+    for i in apts:
+        apartamentos.append({"apartamento":apts[cont].id, "predio": apts[cont].apartamento_idprd})
+        cont = cont+1
+
+
+    return jsonify(apartamentos)
+
+@bp_apartamento.route('/apartamento/mostrar/<id>', methods=['GET'])
+def show_by_id(id):
+    
+    apartamento = ApartamentoTable.query.get(id)
+
+    apt = {
+        "apartamento":apartamento.id,
+        "predio":apartamento.apartamento_idprd
+        }
+
+
+    return jsonify(apt)
+
+@bp_apartamento.route('/apartamento/deletar/<id>', methods=['DELETE'])
+def delete_by_id(id):
+    
+    apartamento = ApartamentoTable.query.get(id)
+
+    db.session.delete(apartamento)
+    db.session.commit()
+
+    return jsonify('Tudo certo')
--- app/run.py
+++ app/run.py
@@ -2,11 +2,12 @@ from __init__ import app
 from api.role.role_routes import bp_role
 from api.users.users_routes import bp_users
 from api.predio.predio_routes import bp_predio
-
+from api.apartamento.apartamento_routes import bp_apartamento
 
 app.register_blueprint(bp_role)
 app.register_blueprint(bp_users)
 app.register_blueprint(bp_predio)
+app.register_blueprint(bp_apartamento)
 
 
 
