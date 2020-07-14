4cab2e8340c3fe83e73ba4856270a6d07eb29aa6
Pedro Lima <pedro.l2002@hotmail.com>
2020-07-14 13:55:54 -0300

    Cors adicionado

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
     app.config.from_pyfile('config.cfg')
 
     return app
--- app/api/sysaccess/sysaccess_routes.py
+++ app/api/sysaccess/sysaccess_routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Blueprint, jsonify
+from flask import Flask, request, Blueprint, jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy
 from __init__ import db
 from models import SysAccessTable
@@ -7,18 +7,18 @@ bp_auth = Blueprint('auth', __name__)
 
 @bp_auth.route('/login', methods=['POST'])
 def login():
+    login = request.json["usuario"]
+    senha = request.json["senha"]
     try:
-        login = request.json['usuario']
-        senha = request.json['senha']
 
         user = SysAccessTable.query.filter_by(login=login, senha=senha).first()
 
         if user:
-            return jsonify({"message":'Bem vindo '+login+'!'})
+            return jsonify({"message":"Bem-vindo "+login+'!', "status":True})
         else:
-            return jsonify({"message":'Acesso negado!'})
+            return jsonify({"message":'Acesso negado!', "status": False, "login":login, "senha": senha})
     except:
-        return jsonify({"message":"Houve um erro ao acessar a conta, verifique as informações inseridas."})
+        return jsonify({"message":"Ocorreu um erro", "status":False, "login": login, "senha": senha})
 
 @bp_auth.route('/cadastrar', methods=['POST'])
 def sign_up():
