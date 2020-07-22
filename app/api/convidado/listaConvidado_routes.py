9c3e9fca95fb416dc600751492a583284f71f33e
Pedro Lima <pedro.l2002@hotmail.com>
2020-07-21 22:33:28 -0300

    Rotas convidados corrigidas

--- app/api/convidado/listaConvidado_routes.py
+++ app/api/convidado/listaConvidado_routes.py
@@ -11,14 +11,10 @@ bp_convidado = Blueprint("convidado", __name__)
 def create():
     nome = request.json['nome']
     rg = request.json['rg']
-    cpforganizador = request.json['cpforganizador']
+    idevt = request.json['id']
 
     try:
-        organizador = PessoaTable.query.filter_by(cpf=cpforganizador).first()
-        organizador = MoradorTable.query.filter_by(morador_idpes=organizador.id).first()
-        evento = EventoTable.query.order_by(EventoTable.id.desc()).filter_by(evento_idmrd=organizador.id).first()
-
-        new_lista = ListaConvidadosTable(nome, rg, evento.id)
+        new_lista = ListaConvidadosTable(nome, rg, idevt)
 
         db.session.add(new_lista)
         db.session.commit()
@@ -28,7 +24,7 @@ def create():
     except:
         return jsonify({"message":'Não foi possível inserir o convidado, favor verificar as informações inseridas.'})
 
-@bp_convidado.route('/convidado/<idevt>/mostrar/', methods=['GET'])
+@bp_convidado.route('/convidado/<idevt>/mostrar', methods=['GET'])
 def show_lista_by_cpf_organizador(idevt):
     evento = EventoTable.query.get(idevt)
 
@@ -48,45 +44,45 @@ def show_lista_by_cpf_organizador(idevt):
         return jsonify(output)
 
     except:
-        return jsonify({"message":'Sem registros. Favor verificar as informações inseridas'})
+       return jsonify({"message":'Sem registros. Favor verificar as informações inseridas'})
 
-@bp_convidado.route('/convidado/<idevt>/mostrar/<rg>', methods=['GET'])
-def show_convidado_by_id(idevt, rg):
+@bp_convidado.route('/convidado/<idevt>/mostrar/<id>', methods=['GET'])
+def show_convidado_by_id(idevt, id):
     evento = EventoTable.query.get(idevt)
     try:
-        convidado = ListaConvidadosTable.query.filter_by(listaconvidados_idevt=evento.id, rg=rg).first() 
+        convidado = ListaConvidadosTable.query.get(id) 
         organizador = MoradorTable.query.get(evento.evento_idmrd)
         organizador = PessoaTable.query.filter_by(id=organizador.morador_idpes).first()
 
-        output = {"id": convidado.id, "nome":convidado.nome,"rg":convidado.rg, "evento": convidado.listaconvidados_idevt, "organizador": organizador.nome}
+        output = {"id": convidado.id, "nome":convidado.nome,"rg":convidado.rg}
         return jsonify(output)
         
     except:
         return jsonify({"message":'Sem registros. Favor verificar as informações inseridas'})
 
 
-@bp_convidado.route('/convidado/<idevt>/alterar/<rg>', methods=['PUT'])
-def modify(idevt, rg):
+@bp_convidado.route('/convidado/<idevt>/alterar/<id>', methods=['PUT'])
+def modify(idevt, id):
     nome = request.json['nome']
-    rgnovo = request.json['rg']
+    rg = request.json['rg']
 
     evento = EventoTable.query.get(idevt)
 
-    convidado = ListaConvidadosTable.query.filter_by(listaconvidados_idevt=evento.id, rg=rg).first()
+    convidado = ListaConvidadosTable.query.get(id)
     
 
     try:
-        if rgnovo != '':
-            convidado.rg = rgnovo
+        if rg != '':
+            convidado.rg = rg
         if nome != '':
             convidado.nome = nome
 
         db.session.commit()
 
-        return jsonify({"message":'Informações do convidado alteradas com sucesso!'})
+        return jsonify({"message":'Informações do convidado alteradas com sucesso!', "status":200})
     
     except:
-        return jsonify({"message":'Não foi possível alterar as informações do convidado.'})
+        return jsonify({"message":'Não foi possível alterar as informações do convidado.', "status":400})
 
 @bp_convidado.route('/convidado/<idevt>/deletar', methods=['DELETE'])
 def delete_lista(idevt):
@@ -110,9 +106,9 @@ def delete_lista(idevt):
     except:
         return jsonify({"message":'Ocorreu um erro ao apagar a lista de convidados, favor verificar as informações inseridas.'})
 
-@bp_convidado.route('/convidado/<idevt>/deletar/<rg>', methods=['DELETE'])
-def delete_convidado(idevt, rg):
-    convidado = ListaConvidadosTable.query.filter_by(listaconvidados_idevt=idevt, rg=rg).first()
+@bp_convidado.route('/convidado/<idevt>/deletar/<id>', methods=['DELETE'])
+def delete_convidado(idevt, id):
+    convidado = ListaConvidadosTable.query.get(id)
 
     try:
         db.session.delete(convidado)
